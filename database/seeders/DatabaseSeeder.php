bd2cc85ec76ce2ae561d2ccb176d02f1a5a79c1a
mayank <mayankpal100@gmail.com>
2020-12-12 17:11:08 +0530

    seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UserDatabaseSeeder::class,
             TaskDatabaseSeeder::class,
+            UsertaskDatabaseSeeder::class,
             
         ]);
     }
